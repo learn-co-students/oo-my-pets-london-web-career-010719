b267238ab84bca0ed3190dc02a90aebb2cb60021
Flatiron <flatiron@Flatirons-MacBook-Air-2.local>
2019-01-10 14:39:34 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,16 @@
+require_relative 'owner.rb'
+require_relative 'cat.rb'
+require_relative 'fish.rb'
+
+
 class Dog
-  # code goes here
+  attr_accessor :mood
+  attr_reader :name
+  # make instance attribute read only
+
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+  end
+# class end
 end
